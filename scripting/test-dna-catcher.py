80c1b1d0e12f8db1f226804b7e8db960fff47336
olivia <oliviamyang@gmail.com>
2019-08-22 19:06:09 -0400

    minor edits

--- scripting/test-dna-catcher.py
+++ scripting/test-dna-catcher.py
@@ -5,6 +5,8 @@ from bluelake import Trap, stage, fluidics, pause, reset_force, timeline
 
 trap = Trap("1", "XY")
 pt=0.3
+echannel=1 #change this to 1 if you want to end in an experiment channel
+dnadwell=1 #how long to dwell in dna channel (sec)
 
 #functions that we need to catch DNA
 def pingpongforce():
@@ -83,7 +85,7 @@ if bt==0:
     setpressure(0.3)
     fluidics.open(1,2,3,6)
     stage.move_to("beads")
-    catch_beads(85)
+    catch_beads(82)
 else:
     #turn on fluidics
     fluidics.open(1, 2, 3, 6)
@@ -101,18 +103,18 @@ pforce_old=pingpongforce()
 pforce=pforce_old
 caught=0
 attempts=1
-print("initialized variables ("+str(round(pforce_old,2)))
+print("initialized variables (old="+str(round(pforce_old,2))+")")
 
 #moving into dna channel and starting pingpong
 while caught!=1:
     print(" we've attempted "+str(attempts)+" times")
-    print("    old force ="+str(round(pforce_old,2)))
+    #print("    new force ="+str(round(pforce,2)))
     stage.move_to("DNA")
-    pause(0.5)
+    pause(dnadwell)
     stage.move_to("buffer")
     pforce=pingpongforce()
-    #print("    new force ="+str(round(pforce)))
-    if (pforce_old*0.8)>pforce or pforce>(pforce_old*1.2):
+    print("    new force ="+str(round(pforce,2)))
+    if (pforce_old*0.77)>pforce or pforce>(pforce_old*1.23):
           caught=1
     else:
           pforce_old=pforce
@@ -127,8 +129,10 @@ stage.move_to("buffer")
 gohome()
 fluidics.stop_flow()
 reset_force()
-print("hopefully in the buffer channel with all reset")
+print("in the buffer channel with all reset")
 
 #if want to move to ch4 to ready for experiment start
-movetoch4()
-trap.move_to(waypoint="stretched",speed=3)
+if echannel==1:
+    movetoch4()
+    trap.move_to(waypoint="stretched",speed=3)
+    print('ready to image!')
