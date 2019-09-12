a8f42a86f9bdc875ec05947334d46bb12da230d3
c-trap computer <oliviamyang@gmail.com>
2019-09-12 18:56:59 -0400

    major dna-catcher2 changes
    -false positive checker
    -prints forces for last 1um
    -finds new beads after 30 attempts

--- scripting/test-dna-catcher2.py
+++ scripting/test-dna-catcher2.py
@@ -2,6 +2,7 @@
 #will catch your beads
 
 from bluelake import Trap, stage, fluidics, pause, reset_force, timeline, traps
+import numpy as np
 
 trap = Trap("1", "XY")
 pt=0.1
@@ -16,16 +17,21 @@ def pingpongforce():
     return ppf
 
 def pingpongforce2():
-    trap.move_by(dx=9.2,speed=5.5)
+    trap.move_by(dx=9,speed=5.5)
     ppf1=trap.current_force
-    trap.move_by(dx=0.8,speed=5.5)
+    move_by2(1.2,5.5)
     ppf2=trap.current_force
-    trap.move_by(dx=-10,speed=6.5)
+    trap.move_by(dx=-10.2,speed=6.5)
     print("  "+str(round(ppf1,2))+" to "+str(round(ppf2,2)))
     if (ppf1*0.77)>ppf2 or ppf2>(ppf1*1.23):
         return 1
     else:
         return 0
+
+def move_by2(x,s): #to print the force every 0.1 step increment
+    for i in np.arange(0,x,0.1):
+        trap.move_by(dx=0.1,speed=s)
+        print(round(trap.current_force,2))
 
 def gohome():
     trap.move_to(waypoint="catch DNA",speed=7)
@@ -89,61 +95,77 @@ def catch_beads(min_score = 30) : #from Zsombor
             pause(1)
     print("beads should be caught")
 
+def false_positive(): #1 if it was ok, 0 if false positive
+    fluidics.close(1,2,3,4,5,6)
+    gohome()
+    pause(0.8)
+    reset_force()
+    trap.move_by(dx=5,speed=6)
+    f1=trap.current_force
+    trap.move_by(dx=5.5,speed=6)
+    f2=trap.current_force
+    trap.move_by(dx=-10.5,speed=6)
+    print("  -FP"+str(round(f1,2))+" to "+str(round(f2,2)))
+    if f2>(1.5*f1):
+        return 1
+    else:
+        return 0
+
+def themainloop(): #will return 1 if caught, 0 if attempted 60 times and failed
+    #check for beads first
+    bt=beadtest()
+    fluidics.close(1,2,3,4,5,6)
+    if bt==0:
+        setpressure(0.3)
+        fluidics.open(1,2,3,6)
+        stage.move_to("beads")
+        catch_beads(86)
+    else:
+        #turn on fluidics
+        fluidics.open(1, 2, 3, 6)
+
+    # set up the pressure
+    stage.move_to("buffer")
+    setpressure(0.22)
+
+    #set traps at initial position and reset force
+    gohome()
+    reset_force()
+    print("initialized position")
+    caught=pingpongforce2()
+    attempts=1
+
+    #moving into dna channel and starting pingpong
+    while caught!=1:
+        print(" we've attempted "+str(attempts)+" times")
+        #print("    new force ="+str(round(pforce,2)))
+        stage.move_to("DNA")
+        pause(dnadwell)
+        stage.move_to("buffer")
+        caught=pingpongforce2();
+        if attempts==30:
+            return 0
+        if (attempts%10)==0:
+            pressurecycle()
+            attempts+=1
+        else:
+            attempts+=1
+
+    print("hopefully we finished")
+    return 1
+
 #THIS IS THE START OF THE MAIN -----------------------------------------
 
-#check for beads first
-bt=beadtest()
-fluidics.close(1,2,3,4,5,6)
-if bt==0:
-    setpressure(0.3)
-    fluidics.open(1,2,3,6)
-    stage.move_to("beads")
-    catch_beads(82)
-else:
-    #turn on fluidics
-    fluidics.open(1, 2, 3, 6)
-
-# set up the pressure
-stage.move_to("buffer")
-setpressure(0.22)
-
-#set traps at initial position and reset force
-gohome()
-reset_force()
-print("initialized position")
-caught=pingpongforce2()
-
-# #initialize variables
-# pforce_old=pingpongforce()
-# pforce=pforce_old
-# caught=0
-attempts=1
-# print("initialized variables (old="+str(round(pforce_old,2))+")")
-
-#moving into dna channel and starting pingpong
-while caught!=1:
-    print(" we've attempted "+str(attempts)+" times")
-    #print("    new force ="+str(round(pforce,2)))
-    stage.move_to("DNA")
-    pause(dnadwell)
-    stage.move_to("buffer")
-    caught=pingpongforce2();
-    if (attempts%15)==0:
-        pressurecycle()
-        attempts+=1
+chk=0
+while chk==0:
+    print("starting loop loop")
+    chk=themainloop()
+    if chk==1:
+        chk=false_positive()
     else:
-        attempts+=1
-    # pforce=pingpongforce()
-    # print("    new force ="+str(round(pforce,2)))
-    # if (pforce_old*0.77)>pforce or pforce>(pforce_old*1.23):
-    #       caught=1
-    # else:
-    #       pforce_old=pforce
-    #       if (attempts%15)==0:
-    #           pressurecycle()
-    #       attempts+=1
-
-print("hopefully we finished")
+        target_traps = traps[:2]
+        target_traps[1].clear()
+        target_traps[0].clear()
 
 #hopefully caught something, move to buffer channel and reset all
 stage.move_to("buffer")
